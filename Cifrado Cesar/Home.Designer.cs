76ee4e56d208b7c53fead8988f4a69c340b47ee7
Oskar <oskarcali18@gmail.com>
2019-10-01 02:34:59 -0500

    Comportamiento UI
    Comportamiento de Carga de la UI

--- Cifrado Cesar/Home.Designer.cs
+++ Cifrado Cesar/Home.Designer.cs
@@ -30,25 +30,26 @@
         {
             this.lblTitle = new System.Windows.Forms.Label();
             this.grpBxCipher = new System.Windows.Forms.GroupBox();
+            this.numUDMove = new System.Windows.Forms.NumericUpDown();
+            this.lblMove = new System.Windows.Forms.Label();
+            this.btnShowHide = new System.Windows.Forms.Button();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.radBtnDiscipher = new System.Windows.Forms.RadioButton();
+            this.radBtnCipher = new System.Windows.Forms.RadioButton();
+            this.cmbBxOptions = new System.Windows.Forms.ComboBox();
+            this.lblOptions = new System.Windows.Forms.Label();
+            this.cmbBxAbc = new System.Windows.Forms.ComboBox();
+            this.lblAbc = new System.Windows.Forms.Label();
             this.toolStripCont = new System.Windows.Forms.ToolStripContainer();
             this.stStrip = new System.Windows.Forms.StatusStrip();
-            this.lblAbc = new System.Windows.Forms.Label();
-            this.cmbBxAbc = new System.Windows.Forms.ComboBox();
-            this.lblOptions = new System.Windows.Forms.Label();
-            this.cmbBxOptions = new System.Windows.Forms.ComboBox();
-            this.radBtnCipher = new System.Windows.Forms.RadioButton();
-            this.radBtnDiscipher = new System.Windows.Forms.RadioButton();
-            this.btnCalculate = new System.Windows.Forms.Button();
-            this.btnShowHide = new System.Windows.Forms.Button();
+            this.toolStripStLblInfo = new System.Windows.Forms.ToolStripStatusLabel();
             this.splitContInfo = new System.Windows.Forms.SplitContainer();
             this.richTxtBxAbc = new System.Windows.Forms.RichTextBox();
             this.splitContCesar = new System.Windows.Forms.SplitContainer();
             this.richTxtBxSource = new System.Windows.Forms.RichTextBox();
             this.richTxtBxResult = new System.Windows.Forms.RichTextBox();
-            this.toolStripStLblInfo = new System.Windows.Forms.ToolStripStatusLabel();
-            this.lblMove = new System.Windows.Forms.Label();
-            this.numUDMove = new System.Windows.Forms.NumericUpDown();
             this.grpBxCipher.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numUDMove)).BeginInit();
             this.toolStripCont.BottomToolStripPanel.SuspendLayout();
             this.toolStripCont.ContentPanel.SuspendLayout();
             this.toolStripCont.SuspendLayout();
@@ -61,14 +62,13 @@
             this.splitContCesar.Panel1.SuspendLayout();
             this.splitContCesar.Panel2.SuspendLayout();
             this.splitContCesar.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numUDMove)).BeginInit();
             this.SuspendLayout();
             // 
             // lblTitle
             // 
             this.lblTitle.Dock = System.Windows.Forms.DockStyle.Top;
             this.lblTitle.Location = new System.Drawing.Point(0, 0);
-            this.lblTitle.Margin = new System.Windows.Forms.Padding(0, 0, 0, 0);
+            this.lblTitle.Margin = new System.Windows.Forms.Padding(0);
             this.lblTitle.Name = "lblTitle";
             this.lblTitle.Size = new System.Drawing.Size(1123, 60);
             this.lblTitle.TabIndex = 0;
@@ -89,12 +89,121 @@
             this.grpBxCipher.Controls.Add(this.lblAbc);
             this.grpBxCipher.Dock = System.Windows.Forms.DockStyle.Top;
             this.grpBxCipher.Location = new System.Drawing.Point(0, 60);
-            this.grpBxCipher.Margin = new System.Windows.Forms.Padding(0, 0, 0, 0);
+            this.grpBxCipher.Margin = new System.Windows.Forms.Padding(0);
             this.grpBxCipher.Name = "grpBxCipher";
             this.grpBxCipher.Size = new System.Drawing.Size(1123, 100);
             this.grpBxCipher.TabIndex = 1;
             this.grpBxCipher.TabStop = false;
             this.grpBxCipher.Text = "Cifrado";
+            // 
+            // numUDMove
+            // 
+            this.numUDMove.Location = new System.Drawing.Point(579, 27);
+            this.numUDMove.Maximum = new decimal(new int[] {
+            0,
+            0,
+            0,
+            0});
+            this.numUDMove.Name = "numUDMove";
+            this.numUDMove.ReadOnly = true;
+            this.numUDMove.Size = new System.Drawing.Size(120, 22);
+            this.numUDMove.TabIndex = 9;
+            // 
+            // lblMove
+            // 
+            this.lblMove.AutoSize = true;
+            this.lblMove.Location = new System.Drawing.Point(502, 29);
+            this.lblMove.Name = "lblMove";
+            this.lblMove.Size = new System.Drawing.Size(71, 17);
+            this.lblMove.TabIndex = 8;
+            this.lblMove.Text = "Desplazo:";
+            // 
+            // btnShowHide
+            // 
+            this.btnShowHide.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnShowHide.Location = new System.Drawing.Point(981, 29);
+            this.btnShowHide.Name = "btnShowHide";
+            this.btnShowHide.Size = new System.Drawing.Size(120, 45);
+            this.btnShowHide.TabIndex = 7;
+            this.btnShowHide.Text = "OCULTAR";
+            this.btnShowHide.UseVisualStyleBackColor = true;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCalculate.Location = new System.Drawing.Point(800, 29);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(130, 45);
+            this.btnCalculate.TabIndex = 6;
+            this.btnCalculate.Text = "CALCULAR";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            // 
+            // radBtnDiscipher
+            // 
+            this.radBtnDiscipher.AutoSize = true;
+            this.radBtnDiscipher.Location = new System.Drawing.Point(348, 63);
+            this.radBtnDiscipher.Name = "radBtnDiscipher";
+            this.radBtnDiscipher.Size = new System.Drawing.Size(86, 21);
+            this.radBtnDiscipher.TabIndex = 5;
+            this.radBtnDiscipher.TabStop = true;
+            this.radBtnDiscipher.Text = "Descifrar";
+            this.radBtnDiscipher.UseVisualStyleBackColor = true;
+            // 
+            // radBtnCipher
+            // 
+            this.radBtnCipher.AutoSize = true;
+            this.radBtnCipher.Location = new System.Drawing.Point(348, 27);
+            this.radBtnCipher.Name = "radBtnCipher";
+            this.radBtnCipher.Size = new System.Drawing.Size(63, 21);
+            this.radBtnCipher.TabIndex = 4;
+            this.radBtnCipher.TabStop = true;
+            this.radBtnCipher.Text = "Cifrar";
+            this.radBtnCipher.UseVisualStyleBackColor = true;
+            // 
+            // cmbBxOptions
+            // 
+            this.cmbBxOptions.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbBxOptions.FormattingEnabled = true;
+            this.cmbBxOptions.Items.AddRange(new object[] {
+            "Minusculas",
+            "Mayusculas",
+            "Ambas"});
+            this.cmbBxOptions.Location = new System.Drawing.Point(117, 62);
+            this.cmbBxOptions.Name = "cmbBxOptions";
+            this.cmbBxOptions.Size = new System.Drawing.Size(140, 24);
+            this.cmbBxOptions.TabIndex = 3;
+            this.cmbBxOptions.SelectedIndexChanged += new System.EventHandler(this.CmbBxOptions_SelectedIndexChanged);
+            // 
+            // lblOptions
+            // 
+            this.lblOptions.AutoSize = true;
+            this.lblOptions.Location = new System.Drawing.Point(15, 65);
+            this.lblOptions.Name = "lblOptions";
+            this.lblOptions.Size = new System.Drawing.Size(72, 17);
+            this.lblOptions.TabIndex = 2;
+            this.lblOptions.Text = "Opciones:";
+            // 
+            // cmbBxAbc
+            // 
+            this.cmbBxAbc.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbBxAbc.FormattingEnabled = true;
+            this.cmbBxAbc.Items.AddRange(new object[] {
+            "Español",
+            "Ingles"});
+            this.cmbBxAbc.Location = new System.Drawing.Point(117, 26);
+            this.cmbBxAbc.Name = "cmbBxAbc";
+            this.cmbBxAbc.Size = new System.Drawing.Size(140, 24);
+            this.cmbBxAbc.TabIndex = 1;
+            this.cmbBxAbc.SelectedIndexChanged += new System.EventHandler(this.CmbBxAbc_SelectedIndexChanged);
+            // 
+            // lblAbc
+            // 
+            this.lblAbc.AutoSize = true;
+            this.lblAbc.Location = new System.Drawing.Point(15, 29);
+            this.lblAbc.Name = "lblAbc";
+            this.lblAbc.Size = new System.Drawing.Size(84, 17);
+            this.lblAbc.TabIndex = 0;
+            this.lblAbc.Text = "Abecedario:";
             // 
             // toolStripCont
             // 
@@ -125,88 +234,13 @@
             this.stStrip.Size = new System.Drawing.Size(1123, 26);
             this.stStrip.TabIndex = 0;
             // 
-            // lblAbc
+            // toolStripStLblInfo
             // 
-            this.lblAbc.AutoSize = true;
-            this.lblAbc.Location = new System.Drawing.Point(15, 29);
-            this.lblAbc.Name = "lblAbc";
-            this.lblAbc.Size = new System.Drawing.Size(84, 17);
-            this.lblAbc.TabIndex = 0;
-            this.lblAbc.Text = "Abecedario:";
-            // 
-            // cmbBxAbc
-            // 
-            this.cmbBxAbc.FormattingEnabled = true;
-            this.cmbBxAbc.Items.AddRange(new object[] {
-            "Español",
-            "Ingles"});
-            this.cmbBxAbc.Location = new System.Drawing.Point(117, 26);
-            this.cmbBxAbc.Name = "cmbBxAbc";
-            this.cmbBxAbc.Size = new System.Drawing.Size(140, 24);
-            this.cmbBxAbc.TabIndex = 1;
-            // 
-            // lblOptions
-            // 
-            this.lblOptions.AutoSize = true;
-            this.lblOptions.Location = new System.Drawing.Point(15, 65);
-            this.lblOptions.Name = "lblOptions";
-            this.lblOptions.Size = new System.Drawing.Size(72, 17);
-            this.lblOptions.TabIndex = 2;
-            this.lblOptions.Text = "Opciones:";
-            // 
-            // cmbBxOptions
-            // 
-            this.cmbBxOptions.FormattingEnabled = true;
-            this.cmbBxOptions.Items.AddRange(new object[] {
-            "Minusculas",
-            "Mayusculas",
-            "Ambas"});
-            this.cmbBxOptions.Location = new System.Drawing.Point(117, 62);
-            this.cmbBxOptions.Name = "cmbBxOptions";
-            this.cmbBxOptions.Size = new System.Drawing.Size(140, 24);
-            this.cmbBxOptions.TabIndex = 3;
-            // 
-            // radBtnCipher
-            // 
-            this.radBtnCipher.AutoSize = true;
-            this.radBtnCipher.Location = new System.Drawing.Point(348, 27);
-            this.radBtnCipher.Name = "radBtnCipher";
-            this.radBtnCipher.Size = new System.Drawing.Size(63, 21);
-            this.radBtnCipher.TabIndex = 4;
-            this.radBtnCipher.TabStop = true;
-            this.radBtnCipher.Text = "Cifrar";
-            this.radBtnCipher.UseVisualStyleBackColor = true;
-            // 
-            // radBtnDiscipher
-            // 
-            this.radBtnDiscipher.AutoSize = true;
-            this.radBtnDiscipher.Location = new System.Drawing.Point(348, 63);
-            this.radBtnDiscipher.Name = "radBtnDiscipher";
-            this.radBtnDiscipher.Size = new System.Drawing.Size(86, 21);
-            this.radBtnDiscipher.TabIndex = 5;
-            this.radBtnDiscipher.TabStop = true;
-            this.radBtnDiscipher.Text = "Descifrar";
-            this.radBtnDiscipher.UseVisualStyleBackColor = true;
-            // 
-            // btnCalculate
-            // 
-            this.btnCalculate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnCalculate.Location = new System.Drawing.Point(800, 29);
-            this.btnCalculate.Name = "btnCalculate";
-            this.btnCalculate.Size = new System.Drawing.Size(130, 45);
-            this.btnCalculate.TabIndex = 6;
-            this.btnCalculate.Text = "CALCULAR";
-            this.btnCalculate.UseVisualStyleBackColor = true;
-            // 
-            // btnShowHide
-            // 
-            this.btnShowHide.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnShowHide.Location = new System.Drawing.Point(981, 29);
-            this.btnShowHide.Name = "btnShowHide";
-            this.btnShowHide.Size = new System.Drawing.Size(120, 45);
-            this.btnShowHide.TabIndex = 7;
-            this.btnShowHide.Text = "OCULTAR";
-            this.btnShowHide.UseVisualStyleBackColor = true;
+            this.toolStripStLblInfo.Name = "toolStripStLblInfo";
+            this.toolStripStLblInfo.Size = new System.Drawing.Size(1108, 20);
+            this.toolStripStLblInfo.Spring = true;
+            this.toolStripStLblInfo.Text = "Total Caracteres";
+            this.toolStripStLblInfo.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // splitContInfo
             // 
@@ -274,36 +308,6 @@
             this.richTxtBxResult.TabIndex = 0;
             this.richTxtBxResult.Text = "";
             // 
-            // toolStripStLblInfo
-            // 
-            this.toolStripStLblInfo.Name = "toolStripStLblInfo";
-            this.toolStripStLblInfo.Size = new System.Drawing.Size(1108, 20);
-            this.toolStripStLblInfo.Spring = true;
-            this.toolStripStLblInfo.Text = "Total Caracteres";
-            this.toolStripStLblInfo.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            // 
-            // lblMove
-            // 
-            this.lblMove.AutoSize = true;
-            this.lblMove.Location = new System.Drawing.Point(502, 29);
-            this.lblMove.Name = "lblMove";
-            this.lblMove.Size = new System.Drawing.Size(71, 17);
-            this.lblMove.TabIndex = 8;
-            this.lblMove.Text = "Desplazo:";
-            // 
-            // numUDMove
-            // 
-            this.numUDMove.Location = new System.Drawing.Point(579, 27);
-            this.numUDMove.Maximum = new decimal(new int[] {
-            0,
-            0,
-            0,
-            0});
-            this.numUDMove.Name = "numUDMove";
-            this.numUDMove.ReadOnly = true;
-            this.numUDMove.Size = new System.Drawing.Size(120, 22);
-            this.numUDMove.TabIndex = 9;
-            // 
             // formHome
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -314,8 +318,10 @@
             this.Controls.Add(this.lblTitle);
             this.Name = "formHome";
             this.Text = "Home";
+            this.Load += new System.EventHandler(this.FormHome_Load);
             this.grpBxCipher.ResumeLayout(false);
             this.grpBxCipher.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numUDMove)).EndInit();
             this.toolStripCont.BottomToolStripPanel.ResumeLayout(false);
             this.toolStripCont.BottomToolStripPanel.PerformLayout();
             this.toolStripCont.ContentPanel.ResumeLayout(false);
@@ -331,7 +337,6 @@
             this.splitContCesar.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContCesar)).EndInit();
             this.splitContCesar.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.numUDMove)).EndInit();
             this.ResumeLayout(false);
 
         }
--- Cifrado Cesar/Home.cs
+++ Cifrado Cesar/Home.cs
@@ -12,9 +12,101 @@ namespace Cifrado_Cesar
 {
     public partial class formHome : Form
     {
+        public List<char> Abecedario { get; set; }
+        private string _idioma;
+        private string _opcion;
+
+        private char[] _espMayus = new[]
+        {
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+            'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S',
+            'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+        };
+
+        private char[] _espMinus = new[]
+        {
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+            'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's',
+            't', 'u', 'v', 'w', 'x', 'y', 'z'
+        };
+
         public formHome()
         {
             InitializeComponent();
+        }
+
+        private void FormHome_Load(object sender, EventArgs e)
+        {
+            Abecedario = new List<char>();
+
+            cmbBxAbc.SelectedIndex = 0;
+            cmbBxOptions.SelectedIndex = 0;
+
+            radBtnCipher.Checked = true;
+        }
+
+        private void CmbBxAbc_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Abecedario.Clear();
+            _idioma = cmbBxAbc.Text;
+
+            switch (_opcion)
+            {
+                case "Minusculas":
+                    Abecedario.AddRange(_espMinus);
+                    break;
+                case "Mayusculas":
+                    Abecedario.AddRange(_espMayus);
+                    break;
+                case "Ambas":
+                    Abecedario.AddRange(_espMayus);
+                    Abecedario.AddRange(_espMinus);
+                    break;
+                default:
+                    Abecedario.AddRange(_espMinus);
+                    break;
+            }
+
+            if (_idioma.Equals("Ingles"))
+            {
+                Abecedario.Remove('ñ');
+                Abecedario.Remove('Ñ');
+            }
+
+            numUDMove.Maximum = Abecedario.Count - 1;
+            richTxtBxAbc.Text = Abecedario.toAbc();
+        }
+
+        private void CmbBxOptions_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Abecedario.Clear();
+            _opcion = cmbBxOptions.Text;
+
+            switch (_opcion)
+            {
+                case "Minusculas":
+                    Abecedario.AddRange(_espMinus);
+                    break;
+                case "Mayusculas":
+                    Abecedario.AddRange(_espMayus);
+                    break;
+                case "Ambas":
+                    Abecedario.AddRange(_espMayus);
+                    Abecedario.AddRange(_espMinus);
+                    break;
+                default:
+                    Abecedario.AddRange(_espMinus);
+                    break;
+            }
+
+            if (_idioma.Equals("Ingles"))
+            {
+                Abecedario.Remove('ñ');
+                Abecedario.Remove('Ñ');
+            }
+
+            numUDMove.Maximum = Abecedario.Count - 1;
+            richTxtBxAbc.Text = Abecedario.toAbc();
         }
     }
 }
