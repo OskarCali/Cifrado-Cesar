32122e969ecfff1977cf8d16f6205c0a57f71818
Oskar <oskarcali18@gmail.com>
2019-10-01 02:34:39 -0500

    Metodo de Extension Abecedario
    Recupera el string de la lista de char

---
+++ Cifrado Cesar/Extension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cifrado_Cesar
+{
+    static class Extension
+    {
+        public static string toAbc(this List<char> Abecedario)
+        {
+            string abc = "";
+
+            foreach (var caracter in Abecedario)
+            {
+                abc += caracter;
+            }
+
+            return abc;
+        }
+    }
+}
